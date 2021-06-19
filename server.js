006191c2c765b40676ad32f5c5f81f1b47b65b34
suraj patil <sp680199@gmail.com>
2021-06-19 14:47:16 +0530

    port change to 3001

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app= express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 app.use(express.static('public'));//which folder to watch for staticfiles
 app.use(express.json());
 const connectDB=require('./config/db');
